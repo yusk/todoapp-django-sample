b17dc86d2b2051cc64dc693de7b7aa8832b2e2c1
yusk <tdfagamdb@yahoo.co.jp>
2021-07-31 17:01:41 +0900

    cat bulk update task tags

--- main/serializers/task.py
+++ main/serializers/task.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from main.models import Task, Project, project
+from main.models import Task, Project, Tag
 from main.utils import with_method_class
 
 
@@ -8,7 +8,7 @@ class TaskSerializer(serializers.ModelSerializer):
     parent_task_ids = with_method_class(serializers.CharField)(required=False, help_text='e.g. "1,3"')
     child_task_ids = serializers.SerializerMethodField(read_only=True)
     project_ids = with_method_class(serializers.CharField)(required=False, help_text='e.g. "1,3"')
-    tags = serializers.SerializerMethodField(read_only=True)
+    tags = with_method_class(serializers.CharField)(required=False, help_text='e.g. "tag1,tag2"')
 
     class Meta:
         model = Task
@@ -19,6 +19,7 @@ class TaskSerializer(serializers.ModelSerializer):
         self.fields['id'].read_only = True
         self.fields['description'].required = False
         self.fields['done_at'].required = False
+        self.fields['done_at'].read_only = False
         self.fields['deadline'].read_only = True
 
     def create(self, validated_data):
@@ -32,11 +33,21 @@ class TaskSerializer(serializers.ModelSerializer):
             project_ids = validated_data.pop('project_ids')
             project_ids = [int(project_id.strip()) for project_id in project_ids.split(",")]
             projects = list(Project.objects.filter(id__in=project_ids))
+        tags = None
+        if 'tags' in validated_data:
+            tag_names = validated_data.pop('tags')
+            tag_names = [tag_name.strip() for tag_name in tag_names.split(",")]
+            tags = []
+            for name in tag_names:
+                tag, _ = Tag.objects.get_or_create(name=name)
+                tags.append(tag)
         instance = super().create(validated_data)
         if tasks:
             instance.parent_tasks.add(*tasks)
         if projects:
             instance.projects.add(*projects)
+        if tags:
+            instance.tags.add(*tags)
         return instance
 
     def update(self, instance, validated_data):
@@ -45,11 +56,25 @@ class TaskSerializer(serializers.ModelSerializer):
             parent_task_ids = [int(task_id.strip()) for task_id in parent_task_ids.split(",")]
             tasks = list(Task.objects.filter(id__in=parent_task_ids))
             instance.parent_tasks.add(*tasks)
+            tasks = instance.parent_tasks.exclude(id__in=parent_task_ids)
+            instance.parent_tasks.remove(*tasks)
         if 'project_ids' in validated_data:
             project_ids = validated_data.pop('project_ids')
             project_ids = [int(project_id.strip()) for project_id in project_ids.split(",")]
             projects = list(Project.objects.filter(id__in=project_ids))
             instance.projects.add(*projects)
+            projects = instance.projects.exclude(id__in=project_ids)
+            instance.projects.remove(*projects)
+        if 'tags' in validated_data:
+            tag_names = validated_data.pop('tags')
+            tag_names = [tag_name.strip() for tag_name in tag_names.split(",")]
+            tags = []
+            for name in tag_names:
+                tag, _ = Tag.objects.get_or_create(name=name)
+                tags.append(tag)
+            instance.tags.add(*tags)
+            tags = instance.tags.exclude(name__in=tag_names)
+            instance.tags.remove(*tags)
         return super().update(instance, validated_data)
 
     def get_parent_task_ids(self, obj):
