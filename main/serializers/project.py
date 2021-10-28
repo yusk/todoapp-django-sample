dae582ef862829889a46961d281a672e0b26d5d6
yusk <tdfagamdb@yahoo.co.jp>
2021-10-28 23:01:34 +0900

    add done_task_ids & not_done_task_ids to project srlzr

--- main/serializers/project.py
+++ main/serializers/project.py
@@ -5,10 +5,12 @@ from main.models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
     task_ids = serializers.SerializerMethodField()
+    done_task_ids = serializers.SerializerMethodField()
+    not_done_task_ids = serializers.SerializerMethodField()
 
     class Meta:
         model = Project
-        fields = ('id', 'name', 'description', 'task_ids', )
+        fields = ('id', 'name', 'description', 'task_ids', 'done_task_ids', 'not_done_task_ids', )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -17,3 +19,9 @@ class ProjectSerializer(serializers.ModelSerializer):
 
     def get_task_ids(self, obj):
         return [d.no for d in obj.tasks.all()]
+
+    def get_done_task_ids(self, obj):
+        return [d.no for d in obj.tasks.all().filter(done_at__isnull=False)]
+
+    def get_not_done_task_ids(self, obj):
+        return [d.no for d in obj.tasks.all().filter(done_at__isnull=True)]
