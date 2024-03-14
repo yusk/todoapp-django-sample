9bae897acb1b06d1963b8863a3e109a5323eaba1
yusk <tdfagamdb@yahoo.co.jp>
2024-03-14 20:37:25 +0900

    update python & clean poetry

--- main/helpers/__init__.py
+++ main/helpers/__init__.py
@@ -1,2 +1,1 @@
 from .jwt import *
-from .stripe import *
--- main/utils/mecab.py
+++
@@ -1,7 +0,0 @@
-import MeCab
-
-
-def tokenize(text):
-    wakati = MeCab.Tagger("-O wakati")
-    wakati.parse("")
-    return wakati.parse(text).strip().split()
