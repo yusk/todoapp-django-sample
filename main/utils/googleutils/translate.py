60669a32787ab78e07d5bab0f87a116adce836b1
yusk <tdfagamdb@yahoo.co.jp>
2024-11-16 16:50:54 +0900

    rm google-cloud-translate

--- main/utils/googleutils/translate.py
+++ main/utils/googleutils/translate.py
@@ -1,27 +1,29 @@
 import requests
 
-from google.cloud import translate as google_translate
+# from google.cloud import translate as google_translate
 
 
-def translate(text, source='ja', target='en', method="gas"):
-    if method == "gas":
-        return translate_with_gas(text=text, source=source, target=target)
+# def translate(text, source="ja", target="en", method="gas"):
+#     if method == "gas":
+#         return translate_with_gas(text=text, source=source, target=target)
 
-    translate_client = google_translate.Client()
+#     translate_client = google_translate.Client()
 
-    translation = translate_client.translate(text, target_language=target)
+#     translation = translate_client.translate(text, target_language=target)
 
-    return translation['translatedText']
+#     return translation["translatedText"]
 
 
-def translate_with_gas(text, source='ja', target='en'):
+def translate_with_gas(text, source="ja", target="en"):
     url = "https://script.google.com/macros/s/AKfycbweJFfBqKUs5gGNnkV2xwTZtZPptI6ebEhcCU2_JvOmHwM2TCk/exec"
     res = requests.get(
-        url, params={
+        url,
+        params={
             "text": text,
             "source": source,
             "target": target,
-        })
+        },
+    )
 
     res.raise_for_status()
     return res.text
